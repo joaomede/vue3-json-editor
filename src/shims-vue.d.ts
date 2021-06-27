0f472eb553620eba501435c8a3d24c763958aeca
augusto <gutim160@gmail.com>
2021-06-27 13:18:26 -0300

    Ajustes

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -4,9 +4,3 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
-
-declare module '*.svg' {
-  import type { DefineComponent } from 'vue';
-  const component: DefineComponent;
-  export default component;
-}
